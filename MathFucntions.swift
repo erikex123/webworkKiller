c30c27ac36e65e1f72544cde121b4d79d9362558
エリック <gtsang@Tsangs-MacBook.local>
2017-04-08 11:13:10 +0800

    this is the first version

---
+++ MathFucntions.swift
@@ -0,0 +1,24 @@
+//
+//  MathFucntions.swift
+//  WebWorkKiller
+//
+//  Created by エリック on 2017/04/07.
+//  Copyright © 2017年 エリック. All rights reserved.
+//
+
+import UIKit
+
+class MathFucntions: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ WebWorkKiller/mathFunctionsEnum.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  WebWorkKiller
+//
+//  Created by エリック on 2017/04/07.
+//  Copyright © 2017年 エリック. All rights reserved.
+//
+
+import Foundation
